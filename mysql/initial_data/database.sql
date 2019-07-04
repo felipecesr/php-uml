0118ba9dbe4357f89190d4471382c4a0d8920896
Felipe César <felipecesr@gmail.com>
2019-07-04 11:09:58 -0300

    Add produtos database table

--- mysql/initial_data/database.sql
+++ mysql/initial_data/database.sql
@@ -1,14 +1,42 @@
-CREATE DATABASE `blog`;
-USE `blog`;
+CREATE DATABASE `testedb`;
+USE `testedb`;
 
 DROP TABLE IF EXISTS `categorias`;
+DROP TABLE IF EXISTS `produtos`;
+DROP TABLE IF EXISTS `produtos_categorias`;
 
 CREATE TABLE IF NOT EXISTS categorias (
-    id INT AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     nome VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
-)  ENGINE=INNODB;
+);
+
+CREATE TABLE IF NOT EXISTS produtos (
+    id INT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    preco FLOAT(10,2),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS produtos_categorias (
+    produto_id INT NOT NULL,
+    categoria_id INT NOT NULL,
+    PRIMARY KEY (produto_id, categoria_id),
+    FOREIGN KEY (produto_id) REFERENCES produtos (id),
+    FOREIGN KEY (categoria_id) REFERENCES categorias (id)
+);
 
 INSERT INTO categorias (id, nome) VALUES
 (1, 'Informatica'),
 (2, 'Escritorio');
+
+INSERT INTO produtos (id, nome, preco) VALUES
+(1, 'Computador', 2000),
+(2, 'Impressora', 800),
+(3, 'Mouse', 80);
+
+INSERT INTO produtos_categorias (produto_id, categoria_id) VALUES
+(1, 1),
+(2, 1),
+(2, 2),
+(3, 1);
