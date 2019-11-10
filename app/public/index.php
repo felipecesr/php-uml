29aef41a9e30772a5965fc1fb6f479c4b980a694
Felipe César <jeanfelipecc@gmail.com>
2019-11-10 13:06:54 -0300

    Add header

--- app/public/index.php
+++ app/public/index.php
@@ -4,6 +4,8 @@ use Controller\CategoriaController;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+header('Content-type: application/json; charset=utf-8');
+
 if ($_SERVER['REQUEST_URI'] === '/categorias' && $_SERVER['REQUEST_METHOD'] === 'GET') {
     $controller = new CategoriaController();
     $result = $controller->listar();
